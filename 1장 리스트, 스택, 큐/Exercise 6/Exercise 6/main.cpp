ad1183ff5ff5da4b8e92c1646f45d7bd78c4cc64
youngSeok0413 <youngSeok0413@gmail.com>
2022-07-14 13:09:20 +0900

    Exercise 6

---
+++ 1장 리스트, 스택, 큐/Exercise 6/Exercise 6/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+
+int main() {
+	std::list<int> list1 = {1, 2, 3, 4};
+	list1.push_back(6);
+	list1.insert(list1.begin(), 0);
+	list1.insert(list1.end(), 7);
+
+	list1.pop_back();
+
+	std::cout << "삽입 & 삭제 후 리스트: ";
+		for (auto i : list1)
+			std::cout << i << " ";
+	return 0;
+}
