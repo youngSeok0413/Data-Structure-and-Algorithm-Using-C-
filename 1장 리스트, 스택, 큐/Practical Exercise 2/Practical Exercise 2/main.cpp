51ed9bba63808594b425b18374655838b6bb7344
youngSeok0413 <youngSeok0413@gmail.com>
2022-07-18 23:13:23 +0900

    Pracitical Exercise 2

--- 1장 리스트, 스택, 큐/Practical Exercise 2/Practical Exercise 2/main.cpp
+++ 1장 리스트, 스택, 큐/Practical Exercise 2/Practical Exercise 2/main.cpp
@@ -11,42 +11,183 @@
 */
 
 #include <iostream>
-#include <vector>
+#include <list>
+#include <algorithm>
+#include <time.h>
 
-enum CARD {
-	ACE = 1, 
-	TWO = 2,
-	THREE = 3,
-	FOUR = 4,
-	FIVE = 5,
-	SIX = 6,
-	SEVEN =7,
-	EIGHT = 8,
-	NINE = 9,
-	TEN = 10,
-	JACK = 11,
-	QUEEN = 12,
-	KING = 13
-};
-
-class player {
+class Player {
 private:
-	bool turn;
-	std::vector<CARD> deck;
+	std::list<int> deck;
 public:
-	player() {
-		turn = false;
-		deck = {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+	Player() {
+		deck = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+		//shuffleDeck();
 	}
 
-	CARD showCard() {
-		return deck[0];
+	int showCard() { return deck.back(); }
+
+	void deleteCard() { deck.pop_back(); }
+
+	void returnCard() {
+		int card = deck.back();
+		deck.pop_back();
+		deck.push_front(card);
 	}
 
-	void shuffleTheDeck() {
+	void shuffleDeck() {
+		const int sz = (const int)(this->deck.size());
+		int arrayBuffer[sz];
+	}
+
+	bool whetherDeckEmpty() { return deck.empty(); }
+
+	void printTheDeck() {
+		for (std::list<int>::iterator iter = deck.begin(); iter != deck.end(); iter++) {
+			std::cout << *iter << " ";
+		}
+
+		std::cout << std::endl;
 	}
 };
+
+class Game {
+private:
+	Player players[4];
+	int whatPlayerShow[4] = {-1, -1, -1, -1};
+	bool deleteOrReturn[4] = {false, false, false, false};
+public:
+	Game() {
+		bool over = false;
+
+		while (!over) {
+			showDeckState();
+			updateWhatPlayerShow();
+			showWhatPlayersShow();
+			updateDeck(updateWheterDeleteOrShow());
+			std::cout << std::endl;
+			over = whehterGameFinished();
+		}
+	}
+
+	void showDeckState() {
+		for (int i = 0; i < 4; i++)
+			players[i].printTheDeck();
+	}
+
+	void showWhatPlayersShow() {
+		for (int i = 0; i < 4; i++)
+			std::cout << "Player " << i + 1 << " : " << whatPlayerShow[i] << std::endl;
+	}
+
+	void showDeleteOrReturn() {
+		for (int i = 0; i < 4; i++)
+			std::cout << "Player " << i + 1 << " : " << deleteOrReturn[i] << std::endl;
+	}
+
+	void updateWhatPlayerShow() {
+		for (int i = 0; i < 4; i++)
+			whatPlayerShow[i] = players[i].showCard();
+	}
+
+	bool updateWheterDeleteOrShow() {
+
+		for (int i = 0; i < 4; i++)
+			deleteOrReturn[i] = false;
+
+		if (whatPlayerShow[0] == whatPlayerShow[1]) {
+			deleteOrReturn[0] = true;
+			deleteOrReturn[1] = true;
+		}
+		if (whatPlayerShow[0] == whatPlayerShow[2]) {
+			deleteOrReturn[0] = true;
+			deleteOrReturn[2] = true;
+		}
+		if (whatPlayerShow[0] == whatPlayerShow[3]) {
+			deleteOrReturn[0] = true;
+			deleteOrReturn[3] = true;
+		}
+		if (whatPlayerShow[1] == whatPlayerShow[2]) {
+			deleteOrReturn[1] = true;
+			deleteOrReturn[2] = true;
+		}
+		if (whatPlayerShow[1] == whatPlayerShow[3]) {
+			deleteOrReturn[1] = true;
+			deleteOrReturn[3] = true;
+		}
+		if (whatPlayerShow[2] == whatPlayerShow[3]) {
+			deleteOrReturn[2] = true;
+			deleteOrReturn[3] = true;
+		}
+
+		int cnt = 0;
+		for (int i = 0; i < 4; i++){
+			if (deleteOrReturn[i]) { cnt++; }
+		}
+
+		if (cnt == 3) {
+			for (int i = 0; i < 4; i++) {
+				if (deleteOrReturn[i] == true) {
+					int coin = rand()%2;
+					if (coin == 1) {
+						deleteOrReturn[i] = false;
+						break;
+					}
+				}
+			}
+		}
+		else if (cnt == 0) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	void updateDeck(bool whetherShuffle){
+
+		for (int i = 0; i < 4; i++) {
+			if (deleteOrReturn[i] == true) {
+				players[i].deleteCard();
+			}
+			else {
+				players[i].returnCard();
+			}
+		}
+
+		if (whetherShuffle == true) {
+			for (int i = 0; i < 4; i++) {
+				int coin = rand()%2;
+				if (coin == 1) {
+					players[i].shuffleDeck();
+				}
+			}
+		}
+	}
+
+	bool whehterGameFinished() {
+		bool over = false;
+
+		for (int i = 0; i < 4; i++) {
+			if (players[i].whetherDeckEmpty()) {
+				std::cout << "Player " << i + 1 << " wins" << std::endl;
+				over = true;
+			}
+		}
+
+		if (over) {
+			std::cout << std::endl;
+			std::cout << "Last State" << std::endl;
+			showWhatPlayersShow();
+		}
+
+		return over;
+	}
+};
+
+
 
 int main() {
+	Game game;
+	
 	return 0;
 }
